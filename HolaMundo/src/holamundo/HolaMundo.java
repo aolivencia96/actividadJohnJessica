61410402fc400aed63583ecfed538f427f69b39b
Er3ku <Er3ku@Toni-PC>
2016-12-11 22:22:20 +0100

    Version 0.1 del proyecto actividadJohnJessica

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        String ejemplo01    = "sample";
+        String ejemplo02    = "text";
+        String ejemploF     = ejemplo01+ejemplo02;
+        System.out.print(ejemploF);
+    }    
+}
